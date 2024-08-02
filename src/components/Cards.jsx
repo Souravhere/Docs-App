f9c9debb52bfc49df1cfd9254cae17e53d287a51
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-08-02 22:02:12 +0530

    Bugs fixed and worked on UX

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import { LuFileSignature } from "react-icons/lu";
+import { MdOutlineSimCardDownload } from "react-icons/md";
+import { IoMdCloseCircle } from "react-icons/io";
 
-function Cards() {
+function Cards({ data }) {
   return (
-    <div className='w-52 relative h-64 bg-zinc-700/90 rounded-[20px] p-3'>
+    <div className='w-52 relative min-h-56 max-h-80 bg-zinc-700/90 rounded-[20px] p-3'>
       <LuFileSignature className='text-2xl text-white' />
-      <p className='font-semibold leading-tight mt-3 text-gray-300'>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
-      <p className='text-xs leading-tight mt-2 text-gray-400'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque quas ab iusto itaque harum tempora!</p>
-      <div className='bg-blue-200 absolute h-8 bottom-0 left-0 rounded-b-[20px] w-full'>
+      <p className='font-semibold leading-tight mt-3 text-gray-300'>{data.title}</p>
+      <p className='text-xs leading-tight mt-2 text-gray-400'>{data.description}</p>
+      <div className='flex items-center justify-between mt-4 text-gray-400'>
+        <h5 className='text-lg font-semibold'>{data.filesize}</h5>
+        <span>
+          {data.closedownload ? <IoMdCloseCircle className='text-2xl hover:cursor-pointer' /> : <MdOutlineSimCardDownload className='text-2xl hover:cursor-pointer' />}
+        </span>
+      </div>
+      <div className='bg-blue-200 absolute h-8 bottom-0 left-0 rounded-b-[20px] w-full flex items-center justify-center hover:cursor-pointer hover:bg-blue-100 duration-500'>
+        <div className="tag">
+          <h3 className='text-center font-semibold'>{data.tagdetails.tagtitle}</h3>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Cards
+export default Cards;
--- src/components/Forground.jsx
+++ src/components/Forground.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import Cards from './Cards'
+import React, { useState } from 'react';
+import Cards from './Cards';
 
 function Forground() {
+  const data = [
+    {
+      title: "Drag and Drop 2",
+      description: "Drag and drop is a method of moving computer files.",
+      filesize: "0.9MB",
+      closedownload: false,
+      tagdetails: {
+        isOpen: true,
+        tagtitle: "Download Now",
+        tagColor: "green"
+      }
+    },
+  ];
+
+  useState();
   return (
     <div className="fixed top-0 left-0 z-50 w-full h-full p-5">
-      <Cards/>
+      {/* <Cards/> */}
+      {data.map((item, index) => (
+        <Cards key={index} data={item} />
+      ))}
     </div>
-  )
+  );
 }
 
-export default Forground
+export default Forground;
