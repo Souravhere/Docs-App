4187e5e46bf86b769d699857a397f5d01c6a6251
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-08-02 20:08:20 +0530

    Cards UI bugs fixed

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,7 +3,7 @@ import { LuFileSignature } from "react-icons/lu";
 
 function Cards() {
   return (
-    <div className='w-52 relative h-64 bg-zinc-700 rounded-[20px] p-3'>
+    <div className='w-52 relative h-64 bg-zinc-700/90 rounded-[20px] p-3'>
       <LuFileSignature className='text-2xl text-white' />
       <p className='font-semibold leading-tight mt-3 text-gray-300'>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
       <p className='text-xs leading-tight mt-2 text-gray-400'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque quas ab iusto itaque harum tempora!</p>
