0ccb8426d926da23ca6c59482610b2b3d7623266
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-08-02 20:04:40 +0530

    Cards  UI created

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { LuFileSignature } from "react-icons/lu";
 
 function Cards() {
   return (
-    <div>
-      
+    <div className='w-52 relative h-64 bg-zinc-700 rounded-[20px] p-3'>
+      <LuFileSignature className='text-2xl text-white' />
+      <p className='font-semibold leading-tight mt-3 text-gray-300'>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
+      <p className='text-xs leading-tight mt-2 text-gray-400'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque quas ab iusto itaque harum tempora!</p>
+      <div className='bg-blue-200 absolute h-8 bottom-0 left-0 rounded-b-[20px] w-full'>
+      </div>
     </div>
   )
 }
--- src/components/Forground.jsx
+++ src/components/Forground.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Cards from './Cards'
 
 function Forground() {
   return (
-    <div className="fixed top-0 left-0 z-50 w-full h-full"></div>
+    <div className="fixed top-0 left-0 z-50 w-full h-full p-5">
+      <Cards/>
+    </div>
   )
 }
 
